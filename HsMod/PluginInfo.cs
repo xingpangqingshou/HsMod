cb20a0ecb9e8c785f288494ceb9850c4dd61df34
Pik-4 <88079004+Pik-4@users.noreply.github.com>
2025-05-06 11:26:01 +0800

    8.1.2.0

---
+++ HsMod/PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace HsMod
+{
+    internal class PluginInfo
+    {
+        public const string PLUGIN_GUID = "HsMod";
+        public const string PLUGIN_NAME = "HsMod";
+        public const string PLUGIN_AUTHOR = "Pik_4";
+        public const string PLUGIN_VERSION = "8.1.2.0";
+    }
+}
